5278ba5c81ddcfb3954bf94f1e82eca68285dc12
FranklinOtero10 <franklin.otero6@gmail.com>
2021-03-05 14:02:56 -0600

    Guia 2 ads completa

---
+++ Cinema/Controllers/PeliculasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.Models;
+
+namespace Cinema.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        // GET: Peliculas/Aleatorias
+        public ActionResult Aleatorias()
+        {
+            Pelicula pelicula = new Pelicula() {id = 1, nombre = "Shrek", Genero = "Comedia", AnioLanzamiento = "2001", Clasificacion = "Familiar" };
+
+            //pelicula.nombre = "Shrek"; Otra forma en que se puede añadir
+
+            return View(pelicula);
+        }
+    }
+}
---
+++ Cinema/Models/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models
+{
+    public class Pelicula
+    {
+        public int id { get; set; }
+        public String nombre { get; set; }
+        public String Genero { get; set; }
+        public String AnioLanzamiento { get; set; }
+        public String Clasificacion { get; set; }
+    }
+}
